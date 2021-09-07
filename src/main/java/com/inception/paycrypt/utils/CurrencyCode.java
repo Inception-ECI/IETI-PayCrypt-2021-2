6b9e62a99e3b5d73fda06a71d74b10f8502e5e66
jsr25 <ramosjuans@live.com.ar>
2021-09-06 22:26:31 -0500

    correction of the seventh adjustment requested

--- src/main/java/com/inception/paycrypt/utils/CurrencyCode.java
+++ src/main/java/com/inception/paycrypt/utils/CurrencyCode.java
@@ -12,13 +12,44 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CurrencyCode {
 
+    /**
+     *  The currency code Colombian peso
+     */
     COP("Colombian peso"),
+
+    /**
+     *  The currency code American dollar
+     */
     USD("American dollar"),
+
+    /**
+     *  The currency code Bitcoin
+     */
     BTC("Bitcoin"),
+
+    /**
+     *  The currency code Ethereum
+     */
     ETH("Ethereum"),
+
+    /**
+     *  The currency code Cardano
+     */
     ADA("Cardano"),
+
+    /**
+     *  The currency code Euro
+     */
     EUR("Euro"),
+
+    /**
+     *  The currency code Argentine peso
+     */
     ARS("Argentine peso"),
+
+    /**
+     *  The currency code Brazilian real
+     */
     BRL("Brazilian real");
 
     private final String value;
