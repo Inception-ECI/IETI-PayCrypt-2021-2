fc90cab34809b859d9a2039fa290156c9785ce80
Andres Calderon <andres.calderon@payu.com>
2021-09-06 20:55:22 -0500

    More Document Types added

--- src/main/java/com/inception/paycrypt/utils/DocumentType.java
+++ src/main/java/com/inception/paycrypt/utils/DocumentType.java
@@ -22,7 +22,82 @@ public enum DocumentType {
 	/**
 	 * Cédula de extranjería
 	 */
-	CE(new Country[] {Country.AR, Country.CL, Country.CO, Country.ES, Country.MX, Country.PE, Country.US});
+	CE(new Country[] {Country.AR, Country.CL, Country.CO, Country.ES, Country.MX, Country.PE, Country.US}),
+
+	/**
+	 * Brasil CPF
+	 */
+	CEP(new Country[] {Country.BR}),
+
+	/**
+	 * Cédula identidad CL
+	 */
+	CI(new Country[] {Country.CL}),
+
+	/**
+	 * Brasil CPF
+	 */
+	CPF(new Country[] {Country.BR}),
+
+	/**
+	 * Brasil Social Contract
+	 */
+	SC(new Country[] {Country.BR}),
+
+	/**
+	 * Argentina CUIT
+	 */
+	CUIT(new Country[] {Country.AR}),
+
+	/**
+	 * Clave única registro población MX
+	 */
+	CURP(new Country[] {Country.MX}),
+
+	/**
+	 * Documento de Identificación Extranjero.
+	 */
+	DE(new Country[] {Country.CO}),
+
+	/**
+	 * Argentina DNI
+	 */
+	DNI(new Country[] {Country.PE, Country.AR, Country.ES}),
+
+	/**
+	 * Número identificación fiscal ES
+	 */
+	NIF(new Country[] {Country.ES}),
+
+	/**
+	 * N.I.T.
+	 */
+	NIT(new Country[] {Country.CO}),
+
+	/**
+	 * Pasaporte.
+	 */
+	PP(new Country[] {Country.AR, Country.CL, Country.PE, Country.ES, Country.MX, Country.US, Country.CO}),
+
+	/**
+	 * Registro mercantil AR
+	 **/
+	RM(new Country[] {Country.AR}),
+
+	/**
+	 * Registro mercantil central ES
+	 **/
+	RMC(new Country[] {Country.ES}),
+
+	/**
+	 * Peru RUC
+	 */
+	RUC(new Country[] {Country.PE}),
+
+	/**
+	 * Social Security Number US
+	 **/
+	SSN(new Country[] {Country.US});
 
 	/**
 	 * Countries that have the DocumentType
