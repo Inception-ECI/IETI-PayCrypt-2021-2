0d7069834cdc2d20820fec20a58cab5bfea51dfb
jsr25 <ramosjuans@live.com.ar>
2021-09-06 22:11:12 -0500

    correction of the second adjustment requested

--- src/main/java/com/inception/paycrypt/dto/CurrencyDto.java
+++ src/main/java/com/inception/paycrypt/dto/CurrencyDto.java
@@ -1,6 +1,8 @@
 package com.inception.paycrypt.dto;
 
 import com.inception.paycrypt.utils.CurrencyCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -11,6 +13,8 @@ import java.util.Date;
  * @version 1.0.0
  * @since 1.0.0
  */
+@Getter
+@NoArgsConstructor
 public class CurrencyDto {
     private String name;
 
@@ -21,44 +25,5 @@ public class CurrencyDto {
     private Date creationDate;
 
     private Date modificationDate;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public CurrencyCode getCurrencyCode() {
-        return currencyCode;
-    }
-
-    public void setCurrencyCode(CurrencyCode currencyCode) {
-        this.currencyCode = currencyCode;
-    }
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public Date getModificationDate() {
-        return modificationDate;
-    }
-
-    public void setModificationDate(Date modificationDate) {
-        this.modificationDate = modificationDate;
-    }
+    
 }
