3f272db22a571a721fc5fec7e74dfc5a62f2c1c1
Andres Calderon <andres.calderon@payu.com>
2021-09-11 20:12:11 -0500

    Creation of Security Configuration class

---
+++ src/main/java/com/inception/paycrypt/configuration/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.inception.paycrypt.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.cors().and().csrf().disable()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/v1/health").permitAll()
+			.antMatchers(HttpMethod.POST, "/v1/user").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+}
