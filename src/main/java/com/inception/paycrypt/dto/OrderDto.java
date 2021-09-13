6c39a30eb71fefde35407aa1a603b4e21dadd484
lale1507 <laurisaleja15.15@gmail.com>
2021-09-12 22:27:01 -0500

    documentation

--- src/main/java/com/inception/paycrypt/dto/OrderDto.java
+++ src/main/java/com/inception/paycrypt/dto/OrderDto.java
@@ -19,13 +19,44 @@ import java.util.Date;
 @NoArgsConstructor
 public class OrderDto {
 
+    /**
+     *  ID orden
+     */
     private String id;
+
+    /**
+     * Target currency of order
+     */
     private int targetCurrency_id;
+
+    /**
+     * Source currency of order
+     */
     private SourceCurrency sourceCurrency_id;
+
+    /**
+     * Target value of order
+     */
     private String targetValue;
+
+    /**
+     * Source value of order
+     */
     private String sourceValue;
+
+    /**
+     * Payment method of order
+     */
     private PaymentMethod paymentMethod_id;
+
+    /**
+     * Date expiration of order
+     */
     private Date expirationDate;
+
+    /**
+     * Date creation of order
+     */
     private Date creationDate;
 
 }
