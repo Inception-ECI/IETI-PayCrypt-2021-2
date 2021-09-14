ea266df775782d9baf7fda11ca2111293c27f586
jsr25 <ramosjuans@live.com.ar>
2021-09-13 23:37:25 -0500

    creation of conversion dto

---
+++ src/main/java/com/inception/paycrypt/dto/RequestConversionDto.java
@@ -0,0 +1,25 @@
+package com.inception.paycrypt.dto;
+
+import com.inception.paycrypt.utils.CurrencyCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+/**
+ *  Class to mapping conversion
+ *
+ * @author Juan Ramos
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+@NoArgsConstructor
+public class RequestConversionDto {
+    private CurrencyCode sourceCurrency;
+
+    private CurrencyCode targetCurrency;
+
+    private double sourceValue;
+
+    private double targetValue;
+}
