4b2d6a1c0702167523e7166be7efd07e71756f6a
jsr25 <ramosjuans@live.com.ar>
2021-09-15 00:38:57 -0500

    Modification of service for conversion processing and api use

--- src/main/java/com/inception/paycrypt/service/ConversionService.java
+++ src/main/java/com/inception/paycrypt/service/ConversionService.java
@@ -1,8 +1,11 @@
 package com.inception.paycrypt.service;
 
 import com.inception.paycrypt.dto.RequestConversionDto;
+import com.inception.paycrypt.dto.ResponseConversionDto;
+
+import java.io.IOException;
 
 public interface ConversionService {
 
-     double conversionCurrency(RequestConversionDto request);
+     ResponseConversionDto conversionCurrency(RequestConversionDto request) throws IOException;
 }
--- src/main/java/com/inception/paycrypt/service/impl/ConversionServiceImpl.java
+++ src/main/java/com/inception/paycrypt/service/impl/ConversionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.inception.paycrypt.service.impl;
 
 import com.inception.paycrypt.dto.RequestConversionDto;
+import com.inception.paycrypt.dto.ResponseConversionDto;
 import com.inception.paycrypt.service.ConversionService;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -8,10 +9,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @Service
 public class ConversionServiceImpl implements ConversionService {
@@ -20,26 +23,31 @@ public class ConversionServiceImpl implements ConversionService {
     private String currencyUrl;
 
     @Override
-    public double conversionCurrency(RequestConversionDto request) {
-       HttpResponse response = requestToApi(request.getSourceCurrency().name(),request.getTargetCurrency().name());
-       if(response!=null){
-        HttpEntity entity = response.getEntity();
-           try {
-               String responseString = EntityUtils.toString(entity, "UTF-8");
-           } catch (IOException e) {
-               e.printStackTrace();
-           }
-       }
-        return 0;
+    public ResponseConversionDto conversionCurrency(RequestConversionDto request) throws IOException {
+        HttpResponse response = requestToApi(request.getSourceCurrency().name(), request.getTargetCurrency().name());
+        ResponseConversionDto responseConversionDto = null;
+        if (response != null) {
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            JSONObject json = new JSONObject(responseString);
+            responseConversionDto=new ResponseConversionDto();
+            responseConversionDto.setCurrency(request.getTargetCurrency());
+            responseConversionDto.setDate(json.get("date").toString());
+            BigDecimal value =(BigDecimal) json.get(request.getTargetCurrency().name().toLowerCase());
+            value =value.multiply(BigDecimal.valueOf(request.getSourceValue()));
+            responseConversionDto.setValue(value);
+        }
+
+        return responseConversionDto;
     }
 
-    private HttpResponse requestToApi(String sourceCurrency, String targetCurrency){
+    private HttpResponse requestToApi(String sourceCurrency, String targetCurrency) {
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        HttpResponse response=null;
-        HttpGet request = new HttpGet(currencyUrl+sourceCurrency.toLowerCase()+"/"+targetCurrency.toLowerCase()+".json");
+        HttpResponse response = null;
+        HttpGet request = new HttpGet(currencyUrl + sourceCurrency.toLowerCase() + "/" + targetCurrency.toLowerCase() + ".json");
         try {
-            response=httpClient.execute(request);
+            response = httpClient.execute(request);
         } catch (IOException e) {
             e.printStackTrace();
         }
