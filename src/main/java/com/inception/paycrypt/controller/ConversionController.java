e5248fc4ad3d245dc80f5c51afc446a600bdabfa
jsr25 <ramosjuans@live.com.ar>
2021-09-15 18:24:35 -0500

    complete controller corrections

--- src/main/java/com/inception/paycrypt/controller/ConversionController.java
+++ src/main/java/com/inception/paycrypt/controller/ConversionController.java
@@ -2,6 +2,7 @@ package com.inception.paycrypt.controller;
 
 import com.inception.paycrypt.dto.RequestConversionDto;
 import com.inception.paycrypt.dto.RequestConversionMore;
+import com.inception.paycrypt.dto.ResponseConversionDto;
 import com.inception.paycrypt.service.ConversionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,29 +13,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+/**
+ * Controller to conversion service
+ *
+ * @author Juan Ramos
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @RestController
 @RequestMapping("/v1/conversion")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ConversionController {
 
-
+    /**
+     * Conversion service variable
+     */
     private ConversionService conversionService;
 
-
+    /**
+     * Method to convert a pair of currency
+     *
+     * @param request variable of the data sent by the user
+     * @return response with conversion data
+     */
     @PostMapping
-    public ResponseEntity<?> conversionOperation(@RequestBody RequestConversionDto request){
-        try{
-            return new ResponseEntity<>(conversionService.conversionCurrency(request),HttpStatus.OK);
-        }catch (Exception e){
+    public ResponseEntity<ResponseConversionDto> conversionOperation(@RequestBody RequestConversionDto request) {
+        try {
+            return new ResponseEntity<>(conversionService.conversionCurrency(request), HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    /**
+     * Method to convert more than two currencies
+     *
+     * @param request variable of the data sent by the user
+     * @return response with list to conversion data
+     */
     @PostMapping("/more")
-    public ResponseEntity<?> conversionCurrency(@RequestBody RequestConversionMore request){
+    public ResponseEntity<List<ResponseConversionDto>> conversionCurrency(@RequestBody RequestConversionMore request) {
         try {
-            return new ResponseEntity<>(conversionService.conversionCurrencyWithMoreCurrency(request),HttpStatus.OK);
-        }catch (Exception e){
+            return new ResponseEntity<>(conversionService.conversionCurrencyWithMoreCurrency(request), HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
