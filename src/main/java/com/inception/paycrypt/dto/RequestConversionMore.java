a35cfc56f06ca17d770c3ecacec325dc06063d00
jsr25 <ramosjuans@live.com.ar>
2021-09-15 18:30:49 -0500

    complete RequestConversionMore corrections

--- src/main/java/com/inception/paycrypt/dto/RequestConversionMore.java
+++ src/main/java/com/inception/paycrypt/dto/RequestConversionMore.java
@@ -4,13 +4,34 @@ package com.inception.paycrypt.dto;
 import com.inception.paycrypt.utils.CurrencyCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+/**
+ *  Mapping class for the Conversion multiples currencys
+ *
+ * @author Juan Ramos
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @Getter
+@Setter
 @NoArgsConstructor
 public class RequestConversionMore {
+
+    /**
+     * List to CurrencyCode
+     */
     private List<CurrencyCode> currencyCodes;
+
+    /**
+     * source currencyCode for service
+     */
     private CurrencyCode sourceCurrency;
+
+    /**
+     * sourceValue for service
+     */
     private double sourceValue;
 }
