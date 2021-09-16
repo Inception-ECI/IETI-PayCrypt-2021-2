72d6ac922188de0ac522aa60fa526dacac2779ad
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-09-15 20:10:29 -0500

    End test of create Postman

--- src/main/java/com/inception/paycrypt/controller/PaymentMethodController.java
+++ src/main/java/com/inception/paycrypt/controller/PaymentMethodController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
  * @since 1.0.0
  */
 @RestController()
-@RequestMapping("/v1/paymentmethod")
+@RequestMapping("/v1/paymentMethod")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PaymentMethodController {
 
